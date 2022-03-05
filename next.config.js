c3e72a616dea2ddb7db3267f7db0f28eeb0c6cf0
Sofía Jiménez Martínez <66180900+dorado5115@users.noreply.github.com>
2022-03-05 13:59:18 -0600

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    AUTH0_SECRET: "vDB3+/U6%\Gh9P_TxCt=KnCQ>`=Mmf?(",
-    AUTH0_BASE_URL: "https://localhost:3000",
-    AUTH0_ISSUER_BASE_URL: "dev-e5pv8qrt.us.auth0.com",
-    AUTH0_CLIENT_ID: "6nW97SjfCp6k1xuqgkaZCDFyOc26fxsz",
-    AUTH0_CLIENT_SECRET: "uVgNVJ7B_0KvP6XlCIpOsz3cd8z7kyojA-gUU9Zeq6eI2tdNScZVlKyuEHNDmYrQ"
-  }
 }
 
 module.exports = nextConfig
